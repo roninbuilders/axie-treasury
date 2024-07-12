d19e727fe871b9a43bfa98feaf5ff24851e34166
Glitch <glitch.txs@gmail.com>
2024-07-13 02:54:41 +0500

    typo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google"
 import { cn } from "@/lib/utils"
 import "./globals.css";
-import Navbar from "@/components/layout/nadvar";
+import Navbar from "@/components/layout/navbar";
 
 const fontSans = Space_Grotesk({
   subsets: ["latin"],
