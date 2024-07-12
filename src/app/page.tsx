81d26ad7e9487d606ade6217ce8adc461f6c0bff
Alex Pedersen <hello@alexpedersen.dev>
2024-07-12 20:56:18 +0200

    Add inflows chart component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import { InflowsChart } from "@/components/ui/inflows-chart";
 import { RevenueChart } from "@/components/ui/revenue-chart";
 import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center p-2">
       <div className="max-w-2xl">
         <h1 className="text-4xl font-bold">Axie Treasury Chart</h1>
 
@@ -12,10 +13,12 @@ export default function Home() {
         </p>
 
         <p className="my-4 text-lg">
-          See how the money comes in from activities like breeding, minting, and marketplace sales.
+          See how the inflows comes in from activities like breeding, minting, and marketplace sales by clicking on the chart below.
         </p>
         <Suspense fallback={<div>Loading...</div>}>
           <RevenueChart />
+          {/* idea: show this chart when click on some day inside the revenuechart */}
+          {/* <InflowsChart /> */}
         </Suspense>
       </div>
     </main>
